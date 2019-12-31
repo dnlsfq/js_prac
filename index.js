91bce07c6cbb262cf7c77e977de48bef7304f730
dnlsfq <dan90@live.com.my>
2020-01-01 01:12:10 +0800

    'updated'

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-let countString = '' ;
-let i = 0;
+// let countString = '' ;
+// let i = 0;
 
-do {
-    countString = countString + i;
-    i++;
-} while(i < 5);
+// do {
+//     countString = countString + i;
+//     i++;
+// } while(i < 5);
 
-console.log(countString);
+// console.log(countString);
+
+
+// const firstMessage = 'I will print!';
+// const secondMessage = 'I will not print!';
+
+// do {
+//     console.log(firstMessage);
+// } while (true === false);
+
+// while(true === false){
+//     console.log(secondMessage);
+// }
+
+for(let i = 0; i < 99; i++){
+    if(i > 2){
+        break;
+    }
+    console.log('Banana.');
+}
+console.log('Orange you glad I broke out the loop!');
